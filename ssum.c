96459f3b41fe17385a1e8e32ea09d3acc3828ff1
Nickolas Yakutovich <n.yakutovich@gmail.com>
2014-03-06 20:16:36 -0500

    Do replacement according to one rule.

--- ssum.c
+++ ssum.c
@@ -8,9 +8,7 @@
 #define MRS 	100
 #define MPNAME 	10
 
-char ruledel = ':';
 char rdel[] = ":";
-char exprdel = ';';
 char edel[] = ";";
 
 
@@ -43,7 +41,7 @@ int getRule(int soffs, char * rules)
 
 	for(i = soffs; i>=0; i--)
 	{
-		if (rules[i] == ruledel) 
+		if (rules[i] == rdel[0]) 
 		{
 			b=i+1;
 			break;
@@ -52,6 +50,74 @@ int getRule(int soffs, char * rules)
 	}
 	// now b is the index of the first byte of rule
 	return b;
+}
+
+// just replace one part to another.
+int applyRule(char * il, char * rule)
+{
+	int rl = strlen(rule);
+	int ril = strlen(il);
+	int rb = (strstr (rule, il) - rule);
+	char rapp[MRS]; // tempopary array to check the rule.
+	char rapp1[MRS];
+	char *t;
+	int i, j;
+
+	if ( (rule[rl-1] == '=') && (rule[rl-2] == '=') )
+	{
+		// now do sybstitution to whole expression in the rule
+		for (i=0; i < rb; i++ )
+		{
+			rapp[i] = rule[i];
+		}
+
+		rapp[i++] = 'X';  // the signature of variable.
+
+
+		for ( j = rb + ril; j < rl ; j++ )
+		{
+			rapp [i + j - rb - ril] = rule [j];
+		}
+
+		rapp [i + j - rb - ril] = 0;
+		
+		printf("substituted: %s\n", rapp);
+
+		// check if we have expression of type "X Y =="
+		t = strtok(rapp, " ");
+		if (t != NULL ) printf("%s\n", t);
+		for (i=0; i<2; i++) {
+			t = strtok(NULL, " ");
+			
+			if (t == NULL)
+			{	
+				printf("It's not 'X Y ==' type of expression, skipping rule.\n" );
+				return -1;
+			}
+			else
+			{
+				printf("%s\n", t);
+
+				if ( (t[0]!= 'X') && (t[0] != '='))		
+				{
+					strcpy(rapp1, t);
+				}
+			}
+		}
+		t = strtok(NULL, " ");
+		if (t != NULL)
+		{
+			printf("It's not 'X Y ==' type of expression, skipping rule.\n" );
+			return -1;
+		}
+
+		// now replacing the expression.
+		strcpy(rule, rapp1);
+		
+		
+	}
+	return 0;
+
 }
 
 int main ()
@@ -82,8 +148,8 @@ int main ()
 	do 
 	{
 		rules[c++] = fgetc(fp);
-		if (rules[c-1] == '\n' ) rules[c-1] = ruledel;
-		if (rules[c-1] == '\r' ) rules[c-1] = ruledel;
+		if (rules[c-1] == '\n' ) rules[c-1] = rdel[0];
+		if (rules[c-1] == '\r' ) rules[c-1] = rdel[0];
 	}
 	while ( (c < MRULES) && (rules[c-1] != EOF) );
 
@@ -102,7 +168,15 @@ int main ()
 	while ( (c < MLEN) && (il[c-1] != '\n') );
 
 	il[c-1] = 0;	
+	//clear trailing spaces
+	c--;
+	while (il [c-1] == ' ' && c>0)	il[--c] = 0;
 
+	if (strlen (il) == 0) 
+	{
+		printf("Empty input, skipping.\n");
+		return 0;
+	}
 	printf("%d\n", strlen(il));
 	printf("%s\n", il );
 
@@ -122,6 +196,10 @@ int main ()
 		crule = strtok( &rules[rsize], rdel );
 
 		printf("crule: %s\n", crule);
+
+		applyRule(il, crule);
+
+		printf("%s\n", crule);
 
 	}
 
